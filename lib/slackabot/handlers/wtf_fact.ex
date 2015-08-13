5b5244d1ac17855ebc441b3000124531cc16b3b2
Nova Tan <nova.tan@rea-group.com>
2015-08-13 14:45:28 +1000

    add wtf fact handler

---
+++ lib/slackabot/handlers/wtf_fact.ex
@@ -0,0 +1,36 @@
+defmodule Slackabot.Handlers.WtfFact do
+  alias Slackabot.Slack
+  alias Utility.Random
+
+  @base_uri "http://wtffunfact.com/page/"
+
+  def handle(message, _) do
+    Slack.msg(%{message | text: image_url})
+  end
+
+  def image_url do
+    HTTPotion.get(url)
+    |> process_response
+  end
+
+  defp process_response(%{status_code: 200, body: body}) do
+    image_path(body)
+  end
+
+  defp url do
+    @base_uri <> page
+  end
+
+  defp page do
+    Random.seed(:os.timestamp)
+    Random.random(800)
+    |> to_string
+  end
+
+  defp image_path(body) do
+    Random.seed(:os.timestamp)
+    Regex.scan(~r/img src=\"(.*)\"\sclass/, body)
+    |> Random.sample
+    |> List.last
+  end
+end
